687ab247ad4fd4dba8edbc2326df0da0ba0bc7f1
FelipeMenezes937 <felipemenezes937@gmail.com>
2026-06-14 17:55:35 -0300

    hotfix(main.py): configurando ambiente de producao

--- backend/main.py
+++ backend/main.py
@@ -459,7 +459,7 @@ def garantir_beneficios_sessao(
 app.add_middleware(
     CORSMiddleware,
     allow_origins=["*"],
-    allow_credentials=True,
+    allow_credentials=False,
     allow_methods=["*"],
     allow_headers=["*"],
 )
